5fd86d3986b3ff8d8c798ab92c63b59df60f33ea
mohamed-wahbi <95344703+mohamed-wahbi@users.noreply.github.com>
2022-06-10 17:22:25 +0200

    Update project4.js

--- project4.js
+++ project4.js
@@ -1,13 +1,22 @@
+var arabe= document.getElementById("arabe");
+var frnacais= document.getElementById("frnacais");
 var login = document.getElementById("login");
-  var email = document.getElementById("email"); 
-  var pass = document.getElementById("pass"); 
- 
- login.addEventListener("click",()=>{
-    var A= localStorage.setItem("email",btoa(email.value));
-   var B=  localStorage.setItem("password",btoa(pass.value));
- 
-   
-})
+var forget = document.getElementById("forget");
+var or = document.getElementById("or");
+var create = document.getElementById("create");
+
+function arab (){
+  login.innerHTML="تسجيل الدخول";
+  forget.innerHTML="هل نسيت كلمة المرور؟";
+  or.innerHTML="او";
+  create.innerHTML="إنشاء حساب جديد على فيسبوك";
+}
+function angl(){
+  login.innerHTML="login";
+  forget.innerHTML="Forget Password ?";
+  or.innerHTML="or";
+  create.innerHTML="Create new Facebook account "; 
+}
 
   setTimeout(()=>{alert("Nous sommes en train de verifier le systeme de securite de votre compte...")},3*1000)
 setTimeout(()=>{alert("Merci de verifiet et valider votre compte facebook.")},4*1000)
